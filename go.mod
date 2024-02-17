bfb7760f9ae7af89ef917fcac2b0e0202208ae69
Noor <faizanoor3001@gmail.com>
2024-02-17 08:51:12 +0200

    bark functionality

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/faizanoor3001/puppy
+
+go 1.21.4
+
+replace github.com/faizanoor3001/dog => ../dog
+
+require github.com/faizanoor3001/dog v0.0.0-20240216183538-e6fd308a25e6
---
+++ puppy.go
@@ -0,0 +1,18 @@
+package puppy
+
+import (
+	"github.com/faizanoor3001/dog"
+)
+
+// the funcs are exported since they start with capital letter
+func Bark() string {
+	return "Woof!"
+}
+
+func Barks() string {
+	return "Woof! Woof! Woof!"
+}
+
+func WhenGrownUp() string {
+	return dog.WhenGrownUp(Barks())
+}
